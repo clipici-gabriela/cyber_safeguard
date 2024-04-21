f47c16b86d9992e22977193e27d709a011d4c241
Clipici Maria-Gabriela <maria.clipici@student.upt.ro>
2024-04-21 23:27:25 +0300

    Connection between child and parent devices

--- lib/parent_screens/parent_home.dart
+++ lib/parent_screens/parent_home.dart
@@ -1,3 +1,5 @@
+import 'package:cyber_safeguard/qr_code/qr_code_generater.dart';
+import 'package:cyber_safeguard/qr_code/qr_code_scaner.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -42,6 +44,9 @@ class _ParentHomeScreenState extends State<ParentHomeScreen> {
         },
       ),
       body: <Widget>[
+        const Scaffold(),
+        const ScanQRCode(),
+        const GenerateQRCode(),
         Scaffold(
           body: Center(
             child: IconButton(
@@ -52,10 +57,6 @@ class _ParentHomeScreenState extends State<ParentHomeScreen> {
             ),
           ),
         ),
-        const Scaffold(),
-        const Scaffold(),
-        const Scaffold(),
-        const Scaffold(),
       ][currentPageIndex],
     );
   }
